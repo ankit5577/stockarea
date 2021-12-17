12c318b8f1b2d57c28f0288055c6412054346499
Ankit Kaushal <kaushalankit101010@gmail.com>
2021-12-17 23:45:02 +0530

    build updates

--- react16/src/components/sidebar/Sidebar.js
+++ react16/src/components/sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Sidebar.module.css";
 
 import { filterWarehousesStore } from "../../services/warehouse/warehouseSlice";
@@ -8,15 +8,16 @@ import { useNavigate } from "react-router";
 function Sidebar(props) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [isChecked, setIsChecked] = useState(false);
 
-  const dispatchFilterWarehouse = (element) => {
-    console.log(element.name, element.value);
-    // dispatch(
-    //   filterWarehousesStore({
-    //     type: "SEARCH_PRODUCT",
-    //     filter: data.trim().toLowerCase(),
-    //   })
-    // );
+  const dispatchFilterWarehouse = () => {
+    setIsChecked(() => !isChecked);
+    dispatch(
+      filterWarehousesStore({
+        type: "AVAILABLE_FILTER",
+        data: !isChecked,
+      })
+    );
   };
 
   return (
@@ -26,18 +27,16 @@ function Sidebar(props) {
       } ${style.sidenavWidth} ${style.sidenav} border`}
     >
       <div className={`${style.subDiv}`}>
-        <h3 className={`${style.subHeading}`}>Location</h3>
-      </div>
-
-      <div className={`${style.subDiv}`}>
-        <h3 className={`${style.subHeading}`}>availability</h3>
+        <h3 className={`${style.subHeading}`}>Live</h3>
         <div className={`${style.checkboxDiv}`}>
           <input
             name="is_available"
             type={"checkbox"}
-            onChange={(e) => dispatchFilterWarehouse(e.target)}
+            value="is_available"
+            checked={isChecked}
+            onChange={(e) => dispatchFilterWarehouse()}
           />{" "}
-          <p className="text-dark-grey"> Available</p>
+          <p className="text-dark-grey">Live</p>
         </div>
       </div>
     </div>
--- react16/src/services/warehouse/warehouseSlice.js
+++ react16/src/services/warehouse/warehouseSlice.js
@@ -8,19 +8,35 @@ export const warehouseSlice = createSlice({
   },
   reducers: {
     filterWarehousesStore: (state, action) => {
-      state.filteredWarehouses = state.originalWarehouses.filter(
-        (warehouse) => {
-          const searchWithin =
-            warehouse.name.trim().toLowerCase() +
-            warehouse.city.trim().toLowerCase() +
-            warehouse.cluster.trim().toLowerCase() +
-            String(warehouse.space_available) +
-            String(warehouse.code) +
-            warehouse.type.trim().toLowerCase();
-          const condition = searchWithin.includes(action.payload.searchterm);
-          return condition;
-        }
-      );
+      console.log(action);
+      switch (action.payload.type) {
+        case "SEARCH_PRODUCT":
+          state.filteredWarehouses = state.originalWarehouses.filter(
+            (warehouse) => {
+              const searchWithin =
+                warehouse.name.trim().toLowerCase() +
+                warehouse.city.trim().toLowerCase() +
+                warehouse.cluster.trim().toLowerCase() +
+                String(warehouse.space_available) +
+                String(warehouse.code) +
+                warehouse.type.trim().toLowerCase();
+              const condition = searchWithin.includes(
+                action.payload.searchterm
+              );
+              return condition;
+            }
+          );
+          break;
+        case "AVAILABLE_FILTER":
+          console.log(action.payload.data);
+          state.filteredWarehouses = state.originalWarehouses.filter(
+            (warehouse) => action.payload.data === warehouse.is_live
+          );
+          break;
+        default:
+          console.log("default redux");
+          break;
+      }
     },
     fetchWarehousesStore: (state, action) => {
       state.originalWarehouses = action.payload.data;
